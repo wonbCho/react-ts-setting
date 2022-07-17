c3e01a8652ac6557b3add4b8089bb362c5dd78cb
wonb <wonb1126@gmail.com>
2022-07-17 21:46:53 +0900

    webpack prod config 수정

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -17,8 +17,8 @@ module.exports = {
     new CopyWebpackPlugin(
       [
         {
-          from: path.resolve(__dirname, '../../assets'),
-          to: path.resolve(__dirname, '../../dist/assets')
+          from: path.resolve(__dirname, '../assets'),
+          to: path.resolve(__dirname, '../dist/assets')
         }
       ]),
   ],
